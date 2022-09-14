22325bd4aed37729601a86378e6cd02199f5b1bf
Vinicius <vinicius.conceicao9@gmail.com>
2022-09-14 15:19:56 -0300

    adding new test cases and updating cypress

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    "reportDir": "cypress/report/mochawesome-report",
+    "overwrite": true,
+    "html": true,
+    "json": false,
+    "timestamp": "mmddyyyy_HHMMss"
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+      
+    },
+    
+  },
+  
+})
---
+++ cypress/e2e/3-Alurapic/alurapic.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+describe('Login e registro de usuarios alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+
+    })
+
+    it('verifica mensagens validacao', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+
+    it('verifica mensagens de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="email"]').type('jaqueline');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+       
+    })
+
+    it('verifica mensagem de letras maiusculas no Username', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="userName"]').type('TESTE');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+       
+    })
+    
+    it('verifica mensagem de Username menor que 2 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="userName"]').type('a');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+       
+    })
+    it('verifica mensagens de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+       
+    })
+})
