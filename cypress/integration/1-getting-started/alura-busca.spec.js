3510206ca6fd2a1b63cff049869fbc7bc6e244a8
Vinicius <vinicius.conceicao9@gmail.com>
2022-09-14 10:18:09 -0300

    Adding new project on git

---
+++ cypress/integration/1-getting-started/alura-busca.spec.js
@@ -0,0 +1,13 @@
+describe('Alura busca cursos', () => {
+    beforeEach(() => {
+        cy.visit('https://www.alura.com.br')
+    })
+
+    it('buscar curso de java', () => {
+        cy.get('#header-barraBusca-form-campoBusca').type('java')
+        cy.get('.header-barraBusca-form-submit').click()
+        cy.get('h4.busca-resultado-nome')
+            .should('contain.text', 'Formação Java e Orientação a Objetos')
+
+    })
+})  
